17570997fa9fb98913b0dfbdfcf1a244b58476e2
Anjali Devakumar <anjalidevakumar@lawn-128-61-33-116.lawn.gatech.edu>
2019-10-18 10:31:08 -0400

    trying this out

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/anjalidevakumar/portfolioweb2.0/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-XXXXXXXX-X","anonymize":true},
+    },{
+      plugin: require('/Users/anjalidevakumar/portfolioweb2.0/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,19 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/src/pages/about.js"))),
+  "component---src-pages-design-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/src/pages/design.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/src/pages/index.js"))),
+  "component---src-pages-proj-1-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/src/pages/proj1.js"))),
+  "component---src-pages-proj-2-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/src/pages/proj2.js"))),
+  "component---src-pages-proj-3-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/src/pages/proj3.js"))),
+  "component---src-pages-proj-4-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/src/pages/proj4.js"))),
+  "component---src-pages-proj-5-js": hot(preferDefault(require("/Users/anjalidevakumar/portfolioweb2.0/src/pages/proj5.js")))
+}
+
